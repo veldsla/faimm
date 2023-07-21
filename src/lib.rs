fd8c4b4f16cd21aec8bed9c9231671a468b0a2c5
Arno Velds <a.velds@nki.nl>
2023-07-21 11:00:35 +0200

    apply rustfmt

--- src/lib.rs
+++ src/lib.rs
@@ -43,24 +43,24 @@
 //! sound implementation) and rust-bio (1.3s same implementation as example)
 //! Some tests show counting can also be improved using simd, but nothing has been released.
 
-use std::path::Path;
 use std::fs::File;
-use std::io::{self, Read, BufRead, BufReader};
+use std::io::{self, BufRead, BufReader, Read};
+use std::path::Path;
 
-use memmap2::{MmapOptions, Mmap};
 use indexmap::IndexSet;
+use memmap2::{Mmap, MmapOptions};
 
 /// The object that stores the parsed fasta index file. You can use it to map chromosome names to
 /// indexes and lookup offsets for chr-start:end coordinates
 #[derive(Debug, Clone)]
 pub struct Fai {
     chromosomes: Vec<FaiRecord>,
-    name_map: IndexSet<String>
+    name_map: IndexSet<String>,
 }
 
 impl Fai {
     /// Open a fasta index file from path `P`.
-    pub fn from_file<P: AsRef<Path>>(path: P) ->io::Result<Self> {
+    pub fn from_file<P: AsRef<Path>>(path: P) -> io::Result<Self> {
         let f = File::open(path)?;
         let br = BufReader::new(f);
 
@@ -72,23 +72,36 @@ impl Fai {
             let p: Vec<_> = line.split('\t').collect();
 
             if p.len() != 5 {
-                return Err(io::Error::new(io::ErrorKind::InvalidData, "Expected 5 columns in .fai file."));
+                return Err(io::Error::new(
+                    io::ErrorKind::InvalidData,
+                    "Expected 5 columns in .fai file.",
+                ));
             }
 
             name_map.insert(p[0].to_owned());
 
-            let ioerr = |e, msg| {
-                io::Error::new(io::ErrorKind::InvalidData, format!("{}:{}", msg, e))
-            };
-            chromosomes.push( FaiRecord {
-                len: p[1].parse().map_err(|e| ioerr(e, "Error parsing chr len in .fai"))?,
-                offset: p[2].parse().map_err(|e| ioerr(e, "Error parsing chr offset in .fai"))?,
-                line_bases: p[3].parse().map_err(|e| ioerr(e, "Error parsing chr line_bases in .fai"))?,
-                line_width: p[4].parse().map_err(|e| ioerr(e, "Error parsing chr line_width in .fai"))?,
+            let ioerr =
+                |e, msg| io::Error::new(io::ErrorKind::InvalidData, format!("{}:{}", msg, e));
+            chromosomes.push(FaiRecord {
+                len: p[1]
+                    .parse()
+                    .map_err(|e| ioerr(e, "Error parsing chr len in .fai"))?,
+                offset: p[2]
+                    .parse()
+                    .map_err(|e| ioerr(e, "Error parsing chr offset in .fai"))?,
+                line_bases: p[3]
+                    .parse()
+                    .map_err(|e| ioerr(e, "Error parsing chr line_bases in .fai"))?,
+                line_width: p[4]
+                    .parse()
+                    .map_err(|e| ioerr(e, "Error parsing chr line_width in .fai"))?,
             });
         }
 
-        Ok(Fai { chromosomes, name_map })
+        Ok(Fai {
+            chromosomes,
+            name_map,
+        })
     }
 
     /// Calculate the slice coordinates (byte offsets).
@@ -98,37 +111,39 @@ impl Fai {
     /// Returns an tuple (start, end) if successful. `io::Error` otherwise.
     #[inline]
     pub fn offset(&self, tid: usize, start: usize, stop: usize) -> io::Result<(usize, usize)> {
-        let chr = &self.chromosomes.get(tid)
-            .ok_or_else(|| io::Error::new(io::ErrorKind::Other, "Chromomsome tid was out of bounds"))?;
+        let chr = &self.chromosomes.get(tid).ok_or_else(|| {
+            io::Error::new(io::ErrorKind::Other, "Chromomsome tid was out of bounds")
+        })?;
         if stop > chr.len {
-            return Err(io::Error::new(io::ErrorKind::Other, "FASTA read interval was out of bounds"));
+            return Err(io::Error::new(
+                io::ErrorKind::Other,
+                "FASTA read interval was out of bounds",
+            ));
         }
 
-        let start_offset = chr.offset
-           + (start / chr.line_bases) * chr.line_width
-           + start % chr.line_bases;
-        let stop_offset = chr.offset
-           + (stop / chr.line_bases) * chr.line_width
-           + stop % chr.line_bases;
+        let start_offset =
+            chr.offset + (start / chr.line_bases) * chr.line_width + start % chr.line_bases;
+        let stop_offset =
+            chr.offset + (stop / chr.line_bases) * chr.line_width + stop % chr.line_bases;
 
         Ok((start_offset, stop_offset))
     }
-    
+
     /// Calculate the slice coordinates (byte offsets).
     /// tid is the index of the chromosome (lookup with `Fai::tid` if necessary.
     ///
     /// Returns an tuple (start, end) if successful. `io::Error` otherwise.
     #[inline]
     pub fn offset_tid(&self, tid: usize) -> io::Result<(usize, usize)> {
-        let chr = &self.chromosomes.get(tid)
-            .ok_or_else(|| io::Error::new(io::ErrorKind::Other, "Chromomsome tid was out of bounds"))?;
+        let chr = &self.chromosomes.get(tid).ok_or_else(|| {
+            io::Error::new(io::ErrorKind::Other, "Chromomsome tid was out of bounds")
+        })?;
         let start_offset = chr.offset;
-        let stop_offset = chr.offset
-           + (chr.len / chr.line_bases) * chr.line_width
-           + chr.len % chr.line_bases;
+        let stop_offset =
+            chr.offset + (chr.len / chr.line_bases) * chr.line_width + chr.len % chr.line_bases;
         Ok((start_offset, stop_offset))
     }
-    
+
     /// Return the index of the chromosome by name in the fasta index.
     ///
     /// Returns the position of chr `name` if succesful, None otherwise.
@@ -141,15 +156,17 @@ impl Fai {
     ///
     /// Returns the size in bases as usize.
     pub fn size(&self, tid: usize) -> io::Result<usize> {
-        let chr = &self.chromosomes.get(tid)
-            .ok_or_else(|| io::Error::new(io::ErrorKind::Other, "Chromomsome tid was out of bounds"))?;
+        let chr = &self.chromosomes.get(tid).ok_or_else(|| {
+            io::Error::new(io::ErrorKind::Other, "Chromomsome tid was out of bounds")
+        })?;
         Ok(chr.len)
     }
 
     /// Return the name of the chromomsome at index tid
     pub fn name(&self, tid: usize) -> io::Result<&String> {
-        self.name_map.get_index(tid)
-            .ok_or_else(|| io::Error::new(io::ErrorKind::Other, "Chromomsome tid was out of bounds"))
+        self.name_map.get_index(tid).ok_or_else(|| {
+            io::Error::new(io::ErrorKind::Other, "Chromomsome tid was out of bounds")
+        })
     }
 
     /// Return the names of the chromosomes from the fasta index in the same order as in the
@@ -160,7 +177,6 @@ impl Fai {
         self.name_map.iter().map(|s| s.as_str()).collect()
     }
 }
-
 
 /// FaiRecord stores the length, offset, and fasta file characterics of a single chromosome
 #[derive(Debug, Clone)]
@@ -174,7 +190,7 @@ pub struct FaiRecord {
 /// The `IndexFasta` can be used to open a fasta file that has a valid .fai index file.
 pub struct IndexedFasta {
     mmap: Mmap,
-    fasta_index: Fai
+    fasta_index: Fai,
 }
 
 impl IndexedFasta {
@@ -196,7 +212,10 @@ impl IndexedFasta {
     /// Returns FastaView for the provided chromsome, start, end if successful, Error otherwise.
     pub fn view(&self, tid: usize, start: usize, stop: usize) -> io::Result<FastaView> {
         if start > stop {
-            return Err(io::Error::new(io::ErrorKind::Other, "Invalid query interval"));
+            return Err(io::Error::new(
+                io::ErrorKind::Other,
+                "Invalid query interval",
+            ));
         }
 
         let (start_byte, stop_byte) = self.fasta_index.offset(tid, start, stop)?;
@@ -204,7 +223,7 @@ impl IndexedFasta {
         Ok(FastaView(&self.mmap[start_byte..stop_byte]))
     }
 
-    /// Use tid to return a view of an entire chromosome. 
+    /// Use tid to return a view of an entire chromosome.
     ///
     /// Returns FastaView for the provided chromsome indicated by tid if successful, Error otherwise.
     pub fn view_tid(&self, tid: usize) -> io::Result<FastaView> {
@@ -221,7 +240,6 @@ impl IndexedFasta {
     }
 }
 
-
 /// A view of a slice of the fasta file bounded by provided coordinates
 pub struct FastaView<'a>(&'a [u8]);
 
@@ -234,7 +252,7 @@ impl<'a> FastaView<'a> {
         let mut bc: BaseCounts = Default::default();
 
         for b in self.bases() {
-            let v:u8 = b << 3;
+            let v: u8 = b << 3;
             if v ^ 8 == 0 {
                 bc.a += 1;
             } else if v ^ 24 == 0 {
@@ -247,7 +265,7 @@ impl<'a> FastaView<'a> {
                 bc.t += 1;
             } else {
                 bc.other += 1;
-             }
+            }
         }
 
         bc
@@ -256,7 +274,7 @@ impl<'a> FastaView<'a> {
     /// Iterator over the bases in the current view. Bases are returned as `u8` representations of
     /// the `char`s in the fasta file. Keep only that chars between 164 and 128 (effectively
     /// skipping newlines)
-    pub fn bases(&self) -> impl Iterator<Item=&'a u8> {
+    pub fn bases(&self) -> impl Iterator<Item = &'a u8> {
         self.0.iter().filter(|&&b| b & 192 == 64)
     }
 }
@@ -272,17 +290,15 @@ impl<'a> Read for FastaView<'a> {
     fn read(&mut self, buf: &mut [u8]) -> io::Result<usize> {
         let mut read = 0;
         let mut skipped = 0;
-        for (t, s) in buf.iter_mut()
-            .zip(self.0.iter().filter(|&&c| {
-                let base = c & 192 == 64;
-                if !base {
-                    skipped += 1;
-                }
-                base
-            }))
-        {
+        for (t, s) in buf.iter_mut().zip(self.0.iter().filter(|&&c| {
+            let base = c & 192 == 64;
+            if !base {
+                skipped += 1;
+            }
+            base
+        })) {
             *t = *s;
-            read +=1;
+            read += 1;
         }
         self.0 = &self.0[(skipped + read)..];
         Ok(read)
@@ -298,13 +314,20 @@ pub struct BaseCounts {
     pub g: usize,
     pub t: usize,
     pub n: usize,
-    pub other: usize
+    pub other: usize,
 }
 
 /// Initialize basecount with zeros
 impl Default for BaseCounts {
     fn default() -> BaseCounts {
-        BaseCounts {a: 0, c: 0, g: 0, t: 0, n: 0, other: 0}
+        BaseCounts {
+            a: 0,
+            c: 0,
+            g: 0,
+            t: 0,
+            n: 0,
+            other: 0,
+        }
     }
 }
 
@@ -330,16 +353,21 @@ mod tests {
         assert_eq!(ir.view(0, 0, 10).unwrap().to_string(), "AAAAAAAAAA");
         assert!(ir.view(0, 0, 11).is_err());
 
-        assert_eq!(ir.view(2, 38, 62).unwrap().to_string(), "CCCCCCCCCCCCGGGGGGGGGGGG");
-        assert_eq!(ir.view(2, 74, 100).unwrap().to_string(), "GTTTTTTTTTTTTTTTTTTTTTTTTT");
+        assert_eq!(
+            ir.view(2, 38, 62).unwrap().to_string(),
+            "CCCCCCCCCCCCGGGGGGGGGGGG"
+        );
+        assert_eq!(
+            ir.view(2, 74, 100).unwrap().to_string(),
+            "GTTTTTTTTTTTTTTTTTTTTTTTTT"
+        );
         assert!(ir.view(0, 120, 130).is_err());
     }
 
     #[test]
     fn view_tid() {
         let ir = IndexedFasta::from_file("test/genome.fa").unwrap();
-        assert_eq!(ir.view_tid(0).unwrap().to_string(),
-            "AAAAAAAAAA");
+        assert_eq!(ir.view_tid(0).unwrap().to_string(), "AAAAAAAAAA");
         assert_eq!(ir.view_tid(1).unwrap().to_string(),
             "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA");
         assert_eq!(ir.view_tid(2).unwrap().to_string(),
@@ -362,7 +390,14 @@ mod tests {
     #[test]
     fn view_counts() {
         let ir = IndexedFasta::from_file("test/genome.fa").unwrap();
-        assert_eq!(ir.view(2, 48, 52).unwrap().count_bases(), BaseCounts {c:2,g:2, ..Default::default()});
+        assert_eq!(
+            ir.view(2, 48, 52).unwrap().count_bases(),
+            BaseCounts {
+                c: 2,
+                g: 2,
+                ..Default::default()
+            }
+        );
     }
 
     #[test]
